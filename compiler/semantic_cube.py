fd3182498d93884cdc05b91a4c8dc72732fb1848
Gustavo Benitez <benitezlgustavo@gmail.com>
2023-04-27 13:52:31 -0600

    Modified Semantic Rules

--- compiler/semantic_cube.py
+++ compiler/semantic_cube.py
@@ -1,40 +1,46 @@
-from utils import print_error
+from compiler.utils import print_error
 
-def get_type(symbol, type1, type2):
-    basic_symbols = ['+', '-', '*']
-    comparison_symbols = ['<', '<=', '>', '>=', '==', '!=']
-    
-    cube = {
-        '=': {
-            'int': {'int': True},
-            'float': {'float': True},
-            'char': {'char': True},
-            'bool': {'bool': True}
-        },
-        '/': {
-            'int': {'int': 'float'},
-            'float': {'float': 'float', 'int': 'float'},
-            'char': {'char': 'float', 'int': 'float'},
-        }
+cube = {
+    '=': {
+        'int': {'int': True, 'float': True},
+        'float': {'float': True},
+        'char': {'char': True},
+        'bool': {'bool': True}
+    },
+    '/': {
+        'int': {'int': 'float'},
+        'float': {'float': 'float', 'int': 'float'}
+    },
+    '&&': {
+        'bool': {'bool': 'bool'}
+    },
+    '||': {
+        'bool': {'bool': 'bool'}
     }
-    
-    for s in basic_symbols:
-        cube[s] = {
-            'int': {'int': 'int', 'float': 'float'},
-            'float': {'float': 'float', 'int': 'float'},
-            'char': {'char': 'int', 'int': 'int', 'float': 'float'}
-        }
-        
-    for s in comparison_symbols:
-        cube[s] = {
-            'int': {'int': 'bool'},
-            'char': {'char': 'bool'},
-            'bool': {'bool': 'bool'}
-        }
-    
-    result_type = cube.get(symbol, {}).get(type1, {}).get(type2)
-    
+}
+
+arithmetic_symbols = ['+', '-', '*']
+comparison_symbols = ['<', '<=', '>', '>=', '==', '!=']
+
+for symbol in arithmetic_symbols:
+    cube[symbol] = {
+        'int': {'int': 'int', 'float': 'float'},
+        'float': {'float': 'float', 'int': 'float'}
+    }
+
+for symbol in comparison_symbols:
+    cube[symbol] = {
+        'int': {'int': 'bool', 'float': 'bool'},
+        'float': {'float': 'bool', 'int': 'bool'},
+        'char': {'char': 'bool'},
+        'bool': {'bool': 'bool'}
+    }
+
+# Function to get the type of the operation result
+def get_type(operand, type1, type2):
+    result_type = cube.get(operand, {}).get(type1, {}).get(type2)
+
     if result_type is None:
-        print_error(f'Cannot perform operation {symbol} to {type1} and {type2}', "EC-01")
+        print_error(f'Cannot perform operation {operand} to {type1} and {type2}', "EC-01")
     else:
         return result_type
